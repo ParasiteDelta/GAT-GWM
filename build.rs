d778098589394818de55be702e604a7db86808b3
ParasiteDelta <3150841-ParasiteDelta@users.noreply.gitlab.com>
2024-09-07 09:19:34 -0600

    0.3.2 - Just auto-formatting via rustfmt

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
 extern crate embed_resource;
 
-fn main() {
-   embed_resource::compile("src/res/icons.rc", embed_resource::NONE);
-}
+fn main() { embed_resource::compile("src/res/icons.rc", embed_resource::NONE); }
