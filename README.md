b76822bd0da1fb7ab685beb5663c8cded535ab6b
ParasiteDelta <3150841-ParasiteDelta@users.noreply.gitlab.com>
2024-09-15 15:43:14 -0600

    Eh, I'm done with this.

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-<p align="center">
-  <img height="100%" src="https://github.com/user-attachments/assets/28415f2c-7e13-428c-b2c7-8a0cfdcb5f27">
-</p>
-
 # GAT - GlazeWM Alternating Tiler, written in Rust.
-
+### Eh, I'm done with this.
+-----
 
 ### Why?
 Really, there was no reason why other than exploring async and websockets using Rust. I haven't been messing around with programming
@@ -14,11 +11,12 @@ And yes, this was initially based on the same premise as [Burgr033's work using 
 ### What about-?
 Yes, I know, you could use pytools to compile [the original script by Burgr033](https://github.com/burgr033/GlazeWM-autotiling-python) and run it as an executable, but I personally hate that approach and think that the packaging for Rust is superior in this regard.
 
-Yes, I know that there's rookie mistakes and a patchy appearance, such as how things were slammed together, and that's because they were. I shamelessly used the general outline from [jonhoo's OBS-DO project](https://github.com/jonhoo/obs-do) to setup async, then tossed on what I could find out there which happened to be a library called [Tungstenite](https://crates.io/crates/tungstenite).
+For proper inspiration, look up [jonhoo's OBS-DO project](https://github.com/jonhoo/obs-do) along with some of his other work.
 
 -----
 ### Okay, so how would I set this up to build from scratch?
-Well, download the source code and make sure that Rust is installed on your system. I tend to use the Nightly toolchain and the GNU GCC branch, which you can download and set as your default with the following:
+Rust@Nightly-GNU:
+
 ```Rust
 rustup install nightly-gnu
 rustup default nightly-gnu
@@ -41,12 +39,3 @@ general:
 ```
 
 -----
-### Wishlist:
-- [X] DONE: Make a way to minimize this to tray or hide the window without crazy code expansion.
-- [ ] Consider expanding tool into offering multiple possible layouts or functions until done by GWM's staff.
-- [ ] Figure out why I'm so bad at programming.
-- [ ] Do better.
-
------
-Hey, you can help or contribute if you want to. Just file a PR.
-My own patchy work is GPLv3, the associated libraries are under their own licenses, assuming it even matters.
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,130 @@
 #![cfg_attr(feature = "no_console", windows_subsystem = "windows")]
+#![allow(unused_labels)]
 
-use anyhow::anyhow;
-use gat_gwm::{
-	error_prompt,
-	get_ws_socket,
-	toggle_tiling_dir,
-	value_from_ws,
-	version_from_ws,
-	window_dimensions_from_value,
-	ws_send,
-	GlazeContainerData,
-	GlazeCurrentTilingData,
-	GlazeEventType,
-	GlazeMajorVersion,
-	GlazeTilingDirection,
-};
+use serde_json::Value;
 use tray_item::{
 	IconSource,
 	TrayItem,
 };
-
-//Left in main due to specificity; non-generic.
-fn build_tray(tray: &mut TrayItem) -> anyhow::Result<()>
-{
-	tray.add_label("GAT - GlazeWM Alternating Tiler")?;
-	let quit_menu_function = || std::process::exit(0);
-	tray.add_menu_item("Quit GAT", quit_menu_function)?;
-	Ok(())
-}
+use tungstenite::{
+	connect,
+	Message,
+};
+use url::Url;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>
 {
-	let _tray: TrayItem = {
-		let mut tray_object = match TrayItem::new(
-			"GAT - GlazeWM Alternating Tiler",
-			IconSource::Resource("main-icon"),
+	let mut tray: TrayItem = TrayItem::new(
+		"GAT - GlazeWM Alternating Tiler",
+		IconSource::Resource("main-icon"),
+	)?;
+	tray.add_label("GAT - GlazeWM Alternating Tiler")?;
+	tray.add_menu_item("Quit GAT", || std::process::exit(0))?;
+
+	let (mut socket, _) = connect(match Url::parse("ws://localhost:6123") {
+		Ok(u) => u,
+		Err(_) => {
+			eprintln!("\nERR: Could not parse registered string to URL, was this mistyped?\n");
+			panic!(
+				"\nMOR: Cannot continue runtime, please double-check your computer configuration!\n"
+			);
+		}
+	})
+	.expect("\nERR: Cant' connect to GWM WS!\n");
+
+	'sub_check: loop {
+		if let Err(_) = socket.send(Message::Text("sub -e focus_changed".into())) {
+			if let Err(_) = socket.send(Message::Text("subscribe -e \"focus_changed\"".into())) {
+				if let Err(_) = socket.send(Message::Text("subscribe -e focus_changed".into())) {
+					panic!(
+						"\nMOR: No known method for subscribing to GWM focus_changed event worked, process suicide!\n"
+					);
+				} else {
+					break 'sub_check;
+				}
+			} else {
+				break 'sub_check;
+			}
+		} else {
+			break 'sub_check;
+		}
+	}
+
+	'focus_sub: loop {
+		let json_msg: Value = match serde_json::from_str(
+			socket
+				.read()
+				.expect("\nERR: Could not read message!\n")
+				.to_text()
+				.unwrap_or_default(),
 		) {
-			Ok(ti) => ti,
+			Ok(v) => v,
 			Err(e) => {
-				error_prompt(
-					"Could Not Init Tray!",
-					"Could not initialize the tray! Aborting...",
-				);
-				return Err(anyhow!("Could not initialize tray!\nRaw error: {e}"));
+				eprintln!("\nERR: GWM WS MSG could not be parsed to Value! Raw error:\n{e}\n");
+				continue;
 			}
 		};
 
-		match build_tray(&mut tray_object) {
-			Ok(()) => tray_object,
-			Err(tie) => {
-				error_prompt(
-					"Could not build tray!",
-					format!("For some reason, could not build the tray.\nRaw error: {tie}")
-						.as_str(),
-				);
-				return Err(tie);
+		let (x, y) = if let Some(x) = json_msg["data"]["focusedContainer"]["width"].as_f64() {
+			if let Some(y) = json_msg["data"]["focusedContainer"]["height"].as_f64() {
+				(x, y)
+			} else {
+				continue;
+			}
+		} else {
+			continue;
+		};
+
+		let tiling_direction: Value = {
+			socket.send(Message::Text("query tiling-direction".into()))?;
+			serde_json::from_str(
+				socket
+					.read()
+					.expect("\nERR: Could not read message!\n")
+					.to_text()
+					.unwrap_or_default(),
+			)?
+		};
+
+		if x < y {
+			if let Err(_) = socket.send(Message::Text(
+				"command set-tiling-direction vertical".into(),
+			)) {
+				if let Err(_) = socket.send(Message::Text(
+					"command \"tiling direction vertical\"".into(),
+				)) {
+					if tiling_direction["data"]["tilingDirection"] == "\"horizontal\"" {
+						let _ =
+							socket.send(Message::Text("command toggle-tiling-direction".into()));
+					} else {
+						continue;
+					}
+				} else {
+					continue;
+				}
+			} else {
+				continue;
+			}
+		} else if x > y {
+			if let Err(_) = socket.send(Message::Text(
+				"command set-tiling-direction horizontal".into(),
+			)) {
+				if let Err(_) = socket.send(Message::Text(
+					"command \"tiling direction horizontal\"".into(),
+				)) {
+					if tiling_direction == "\"vertical\"" {
+						let _ =
+							socket.send(Message::Text("command toggle-tiling-direction".into()));
+					} else {
+						continue;
+					}
+				} else {
+					continue;
+				}
+			} else {
+				continue;
 			}
 		}
-	};
-
-	let mut socket = get_ws_socket();
-	let mut current_data = GlazeCurrentTilingData {
-		rt_version: { GlazeMajorVersion::from(version_from_ws(&mut socket).major) },
-		..GlazeCurrentTilingData::default()
-	};
-
-	//Explicit match, to further restrict runtime validity to quantified values.
-	let focus_runtime_container: GlazeEventType = match current_data.rt_version {
-		GlazeMajorVersion::V3 => GlazeEventType::FocusChanged(GlazeContainerData::new()),
-		GlazeMajorVersion::V2 => GlazeEventType::FocusChanged(GlazeContainerData::new()),
-	};
-
-	//See, this is a good example of why I'm probably just going to stick to the
-	// fundamentals for comparators. Using If Let here causes a false warning from
-	// the compiler, stating that the code after the exit-on-error call is
-	// unreachable when it's not. While I know that this is actually the fault of
-	// the design team, since they've yet to even create a stabilized format, much
-	// less fully incorporate now-standard features, it doesn't make it any less
-	// irritating to deal with.
-	match focus_runtime_container.subscribe_from(&mut socket) {
-		Err(e) => {
-			error_prompt(
-				"Could not subscribe via GWM IPC!",
-				"For some reason, could not subscribe to Event via GWM IPC",
-			);
-			return Err(anyhow!(
-				"Could not parse GWM Subscription data!\nRaw error: {e}"
-			));
-		}
-		_ => loop {
-			let focus_msg = {
-				let buf = value_from_ws(&mut socket);
-				match buf {
-					_ if buf == serde_json::Value::default() => continue,
-					_ => buf,
-				}
-			};
-
-			current_data.tiling_dir = match &current_data.rt_version {
-				GlazeMajorVersion::V3 => {
-					let _ = ws_send(&mut socket, "query tiling-direction".into());
-					let tiling_value = value_from_ws(&mut socket);
-
-					Some(
-						match GlazeTilingDirection::from_str(
-							tiling_value["data"]["directionContainer"]["tilingDirection"]
-								.to_string(),
-						) {
-							Ok(td) => td,
-							Err(_) => continue,
-						},
-					)
-				}
-				GlazeMajorVersion::V2 => None,
-			};
-
-			match window_dimensions_from_value(&focus_msg["data"]["focusedContainer"]) {
-				Some((x, y)) => toggle_tiling_dir(&mut socket, x, y, &current_data)?,
-				None => continue,
-			}
-		},
 	}
 }
